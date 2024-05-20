e87b18a85ac6940b1c33a3d8cd5a8c87dabdd47b
BryanSagbay <bryansagbay2001@hotmail.com>
2024-05-19 19:05:17 -0500

    Add files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AuthGuardService } from './service/auth-guard.service';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EventosComponent } from './pages/eventos/eventos.component';
 import { ConfiguracionComponent } from './pages/configuracion/configuracion.component';
+import { LocalesComponent } from './pages/locales/locales.component';
 
 export const routes: Routes = [
   {
@@ -25,9 +26,25 @@ export const routes: Routes = [
     component: DashboardComponent,
     canActivate: [AuthGuardService],
     children: [
-      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-      { path: 'eventos', component: EventosComponent },
-      { path: 'configuracion', component: ConfiguracionComponent },
+      {
+        path: '',
+        redirectTo: 'locales',
+        pathMatch: 'full' },
+      {
+        path: 'locales',
+        component: LocalesComponent,
+        canActivate: [AuthGuardService],
+      },
+      {
+        path: 'eventos',
+        component: EventosComponent,
+        canActivate: [AuthGuardService],
+      },
+      {
+        path: 'configuracion',
+        component: ConfiguracionComponent,
+        canActivate: [AuthGuardService],
+      },
     ],
   },
 ];
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../service/auth.service';
-import { Locales } from '../../model/Locales';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
@@ -12,58 +11,11 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
-  localForm!: FormGroup;
-  local: Locales | undefined;
-
-  constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) { }
-
-  ngOnInit(): void {
-    this.localForm = this.formBuilder.group({
-      id: ['', Validators.required],
-      nombre: ['', Validators.required],
-      direccion: ['', Validators.required],
-      telefono: ['', Validators.required],
-      logo: ['', Validators.required],
-      categoria: ['', Validators.required]
-    });
-    this.getLocalDetails();
-  }
-
-  // Método para agregar un local
-  onSubmit(): void {
-    if (this.localForm.invalid) {
-      return;
-    }
-
-    this.authService.addLocal(this.localForm.value).subscribe(
-      (response: Locales) => { // Tipa la respuesta como Locales
-        console.log('Local agregado correctamente:', response);
-        // Limpiar el formulario después de agregar el local
-        this.localForm.reset();
-      },
-      error => {
-        console.error('Error al agregar local:', error);
-      }
-    );
-  }
-
-  // Método para obtener los detalles del local
-  getLocalDetails(): void {
-    this.authService.getLocalDetails().subscribe(
-      (data) => {
-        this.local = data;
-        console.log('Detalles del local:', this.local);
-      },
-      (error) => {
-        console.error('Error al obtener los detalles del local:', error);
-      }
-    );
-  }
-
+export class DashboardComponent {
   //Metodo para cerrar sesion
   logout(): void {
     this.authService.logout();
     this.router.navigateByUrl('/login');
   }
+  constructor(private authService: AuthService, private router: Router) {}
 }
