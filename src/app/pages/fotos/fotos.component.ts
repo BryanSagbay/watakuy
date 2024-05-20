05a9725e8bf46567a5b26e86e75912cb93a0088e
BryanSagbay <bryansagbay2001@hotmail.com>
2024-05-20 16:26:55 -0500

    Add files

--- src/app/pages/fotos/fotos.component.ts
+++ src/app/pages/fotos/fotos.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { AuthGuardService } from './../../service/auth-guard.service';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../../service/auth.service';
+import { ImagesLocales } from '../../model/ImagesEvents';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fotos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './fotos.component.html',
   styleUrl: './fotos.component.css'
 })
 export class FotosComponent {
+  localId: number = 0;
+  rutaImagen: string = "";
 
+  constructor(private authService: AuthService) {}
+
+  agregarFoto() {
+    const imagelocal: ImagesLocales = {
+      id_local: this.localId,
+      ruta_imagen: this.rutaImagen
+    };
+
+    this.authService.agregarFotoLocal(imagelocal).subscribe(
+      (response) => {
+        console.log('Foto agregada al local:', response);
+        // Aquí puedes manejar la respuesta, por ejemplo, actualizar la interfaz de usuario.
+      },
+      (error) => {
+        console.error('Error al agregar foto al local:', error);
+        // Aquí puedes manejar el error, por ejemplo, mostrar un mensaje al usuario.
+      }
+    );
+  }
 }
