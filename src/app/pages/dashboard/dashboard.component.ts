8f3e417076e7f6cb588f5d990a7e1ceca7b7eaef
BryanSagbay <bryansagbay2001@hotmail.com>
2024-05-20 23:46:45 -0500

    Add alert in user

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../service/auth.service';
 import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,9 +14,26 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class DashboardComponent {
   //Metodo para cerrar sesion
-  logout(): void {
-    this.authService.logout();
-    this.router.navigateByUrl('/login');
-  }
   constructor(private authService: AuthService, private router: Router) {}
+
+  // Método para cerrar sesión con confirmación
+  confirmLogout(event: Event): void {
+    event.preventDefault(); // Detiene el comportamiento predeterminado del enlace
+
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: '¿Quieres cerrar sesión?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, cerrar sesión',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.authService.logout();
+        this.router.navigateByUrl('/login');
+      }
+    });
+  }
 }
